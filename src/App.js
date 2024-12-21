1cf4e0d69d2a92eeaf93ddf21e5f5fbbef2f1bb3
Amrit Thapa <thapa.amet27@gmail.com>
2024-12-21 20:23:11 +0545

    name changed

--- src/App.js
+++ src/App.js
@@ -4,19 +4,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>Amrit Thapa</div>
-        <div>React Developer</div>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>Code - Software</div>
+        <div>Under development</div>
       </header>
     </div>
   );
